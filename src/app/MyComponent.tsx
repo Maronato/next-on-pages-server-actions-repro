6cba9ee17f0dc9ae48c43b0245b0f0a837cadb23
Gustavo Maronato <gustavo.maronato@klaviyo.com>
2024-02-26 04:21:25 -0500

    pass server action as prop to client component

---
+++ src/app/MyComponent.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useFormState } from "react-dom";
+
+import type { myAction } from "@/actions";
+
+export default function MyComponent({ action }: { action: typeof myAction }) {
+  const [formState, handleSubmit] = useFormState(action, null);
+
+  return (
+    <form
+      action={handleSubmit}
+      className="flex flex-col items-center justify-center min-h-screen py-2"
+    >
+      <span className="text-2xl text-center font-semibold mb-4 dark:text-white lg:text-4xl lg:mb-8">{`Result: ${formState}`}</span>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,24 +1,7 @@
-"use client";
-
-import { useFormState } from "react-dom";
-
+"use server";
 import { myAction } from "@/actions";
+import MyComponent from "../MyComponent";
 
-export default function Hello() {
-  const [formState, handleSubmit] = useFormState(myAction, null);
-
-  return (
-    <form
-      action={handleSubmit}
-      className="flex flex-col items-center justify-center min-h-screen py-2"
-    >
-      <span className="text-2xl text-center font-semibold mb-4 dark:text-white lg:text-4xl lg:mb-8">{`Result: ${formState}`}</span>
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Submit
-      </button>
-    </form>
-  );
+export default async function Hello() {
+  return <MyComponent action={myAction} />;
 }
